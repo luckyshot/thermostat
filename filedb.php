b9ddb5619a8f66422f4ff535610c218d00bad8ca
Xavi Esteve <xavi@xaviesteve.com>
2021-11-29 23:43:20 +0100

    filedb with tempfile mode

--- filedb.php
+++ filedb.php
@@ -3,7 +3,7 @@
 /*
 File Database Class
 Avoid the use of MySQL by storing variables and arrays into readable files
-Version: 1.0.2
+Version: 1.0.3
 Usage:
     require 'filedb.php';
 	$fdb = new FileDB;
@@ -17,7 +17,8 @@ Usage:
 	}
 
 */
-class FileDB {
+class FileDB
+{
 
 	/**
 	 * Read a file containing a PHP variable
@@ -25,14 +26,11 @@ class FileDB {
 	 * @param string $file File name (without extension)
 	 * @return object The PHP variable (or null if the file doesn't exist)
 	 */
-	public function get( $file )
+	public function get($file)
 	{
-		if ( file_exists( dirname(__FILE__) . '/' . $file . '.php' ) )
-		{
-			return include( dirname(__FILE__) . '/' . $file . '.php' );
-		}
-		else
-		{
+		if (file_exists(dirname(__FILE__) . '/' . $file . '.php')) {
+			return include(dirname(__FILE__) . '/' . $file . '.php');
+		} else {
 			return null;
 		}
 	}
@@ -45,9 +43,15 @@ class FileDB {
 	 * @param object $data The PHP variable to save
 	 * @return Number of bytes written (or false on error)
 	 */
-	public function set( $file, $data )
+	public function set($file, $data)
 	{
-		return file_put_contents( dirname(__FILE__) . '/' . $file . '.php', '<?php return ' . var_export( $data, true ) . ';' );
+		$tempfile = dirname(__FILE__) . '/' . $file . '.' . mt_rand(1000000, 9999999) . '.php';
+		// Save as tempfile first so we don't run into race conditions with other processes
+		// This solution will override the database in case of a race condition but at least it won't corrupt it
+		// A better solution would be to lock the DB while in use, making other processes wait
+		$result = file_put_contents($tempfile, '<?php return ' . var_export($data, true) . ';');
+		// Rename to $file
+		rename($tempfile, dirname(__FILE__) . '/' . $file . '.php');
+		return $result;
 	}
-
 }
